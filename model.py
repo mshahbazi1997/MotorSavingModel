c0bc1e811aeaf3e3ab79a9bc798fc0786e2ae99e
Mahdiyar Shahbazi <70170749+mshahbazi1997@users.noreply.github.com>
2023-10-18 22:27:18 -0400

    update muscle loss #try20

--- model.py
+++ model.py
@@ -99,7 +99,7 @@ def train(model_num,ff_coefficient,phase,condition='train',directory_name=None):
     max_iso_force_n = max_iso_force / th.mean(max_iso_force) # th.mean(th.square(max_iso_force)) # th.sum(th.squ)/
     activation_scaled = all_muscle * max_iso_force_n
     d_muscle = th.mean(th.square(th.diff(activation_scaled, axis=1)/env.dt))
-    muscle_loss = 8*(th.mean(th.square(activation_scaled))+0.04*d_muscle) # 5
+    muscle_loss = 5*(th.mean(th.square(activation_scaled))+0.05*d_muscle) # 5
     # hidden_loss
     d_hidden = th.mean(th.square(th.diff(all_hidden, axis=1)/env.dt))
     hidden_loss = 0.1*(th.mean(th.square(all_hidden))+0.05*d_hidden)
